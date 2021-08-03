92b6e826f7d3dc219783ed9585a77fd796a1c017
Vlad Dmitrievitch <me@2tunnels.com>
2021-08-03 15:59:10 +0200

    Add Google Chrome installation for node builder

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@
 set -eu
 
 ROOT_DIR=$(pwd)
+DATE=$(date +%Y.%m.%d)
 IMAGES="python-3.6-slim-buster node-10.24-slim-buster"
 
 for IMAGE in $IMAGES
 do
 	cd "$IMAGE"
-    docker image build -t 2tunnels/cloud-builder-${IMAGE} .
-    docker image push 2tunnels/cloud-builder-${IMAGE}
+    docker image build -t cloud-builder-${IMAGE} .
+    docker image tag cloud-builder-${IMAGE} 2tunnels/cloud-builder-${IMAGE}:${DATE}
+    docker image tag cloud-builder-${IMAGE} 2tunnels/cloud-builder-${IMAGE}:latest
+    docker image push 2tunnels/cloud-builder-${IMAGE}:${DATE}
+    docker image push 2tunnels/cloud-builder-${IMAGE}:latest
     cd "$ROOT_DIR"
 done
